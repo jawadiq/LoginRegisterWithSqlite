841ab1a12ba3f251b5262b428a3ae58125de92ba
jawadiq <jawadiqbal84@gmail.com>
2020-04-06 14:34:34 +0200

    Initial commit

---
+++ app/src/main/java/com/loopwiki/loginregisterwithsqlite/User.java
@@ -0,0 +1,23 @@
+package com.loopwiki.loginregisterwithsqlite;
+
+/**
+ * Created by amardeep on 10/26/2017.
+ */
+
+public class User {
+    public String id;
+    public String userName;
+    public String email;
+    public String password;
+    public int age;
+    public int height;
+    public int  weight;
+
+    public User(String id, String userName, String email, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+}
